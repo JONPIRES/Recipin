71c4bae9aef114ef615750ffbdb30ec26f5b7354
Jonathan Pires <jonbpires@gmail.com>
2025-04-10 15:14:43 -0400

    remove app template

--- recipin-frontend/src/app/Pages/recipes/recipe-list/recipe-list.component.ts
+++ recipin-frontend/src/app/Pages/recipes/recipe-list/recipe-list.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [],
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.scss'
+  styleUrl: './recipe-list.component.scss',
 })
-export class RecipeListComponent {
-
-}
+export class RecipeListComponent {}
--- recipin-frontend/src/app/api-services/recipes-api.service.ts
+++ recipin-frontend/src/app/api-services/recipes-api.service.ts
@@ -1,8 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Recipe } from '../models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipesApiService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  private api = '/api/recipes';
+
+  getAllRecipes() {
+    return this.http.get<Recipe[]>(this.api);
+  }
+
+  createRecipe(recipe: Recipe) {
+    return this.http.post<Recipe>(this.api, recipe);
+  }
+
+  deleteRecipe(id: number) {
+    return this.http.delete(`${this.api}/${id}`);
+  }
 }
